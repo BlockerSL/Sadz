2c09950e1faee4ef9d1d18950e61f36f13e22ef8
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-12-02 21:54:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function sahand(){
+    var Ph = document.getElementById("sahand").value;
+    
+if(AM==='PH'){
+  var H3O =Math.log10(Ph);
+  document.getElementById("H3O1").vlaue =H3O;
+  
+  
+}else{
+  alert("Erore");
+}
+}
