c8016b49f62c63c4df7793ff3b4d3659523b3c68
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-12-02 21:58:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-
 function sahand(){
     var Ph = document.getElementById("sahand").value;
     
 if(AM==='PH'){
   var H3O =Math.log10(Ph);
- alert(H3O);
+  document.getElementById("H3O1").vlaue =H3O;
   
   
 }
+
 }
+
